1e955c056bf8d19e058de022c0fd14cbc73a5b27
Colin <colin.laresebros@gmail.com>
2025-01-13 19:34:32 +0100

    Création de la fenetre en fullscreen selon la résolution de l'user

--- wpm-test.py
+++ wpm-test.py
@@ -1,6 +1,16 @@
-# Librairie à choisire
-import pygame
+import pygame as pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.quit()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption('mpm testeur')
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        pygame.display.update()
