fb0bb4f67669496f54a63c6ad931b469cdaf035c
Colin <colin.laresebros@gmail.com>
2025-01-19 21:54:51 +0100

    réarengement du code, création de l espace d ecriture

--- App/wpm-test.py
+++ App/wpm-test.py
@@ -1,8 +1,10 @@
 import codecs, pygame
 
-# définition des couleurs primaires/principales
-white, black = (255, 255, 255), (0, 0, 0)
-green, blue, red = (0, 255, 0), (0, 0, 255), (255, 0, 0)
+# definition des couleurs primaires/principales
+white, black          = (255, 255, 255), (0, 0, 0)
+red, green, blue      = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+yellow, cyan, magenta = (255, 255, 0), (0, 255, 255), (255, 0, 255)
+orange, purple, pink  = (255, 165, 0), (128, 0, 128), (233, 40, 99)
 
 # Creation de la fenetre noir en plein ecran
 pygame.init()
@@ -10,62 +12,73 @@ window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 window.fill(black) 
 pygame.display.set_caption('MPM testeur')
 
-# dimension x et y de l'ecran de l'utilisateur
-screen_width = pygame.display.Info().current_w
-screen_height = pygame.display.Info().current_h 
-
-def xy_pourcent(X=0, Y=0): return int(screen_width*X*0.01), int(screen_height*Y*0.01)# x, y convertit selon de l'écran
-
-def police(nom_police, taille): return pygame.font.SysFont(nom_police, taille) # Police d'ecriture
-
-# L'utilisateur choisi sa langue = selection d'un fichier en consequence
-def choisir_langue(langue):
-    global mots # pouvoir acceder a la liste mots partout
-    fichiertxt = f"Langues/{langue}.txt"
-    with codecs.open(fichiertxt, encoding = 'utf-8') as fichier:
-        mots = [ligne[:-1] for ligne in fichier.readlines()]
-
+######## Fonctions pour alleger le code ########
+elements = {}
+def creer_label(nom, couleur, x, y, w, h):
+    nom_xy, nom_wh = nom+"_xy", nom+"_wh"
+    globals()[nom_xy] = xy_pourcent(x, y)
+    globals()[nom_wh] = xy_pourcent(w, h)
+    globals()[nom] = pygame.Surface(globals()[nom_wh])
+    globals()[nom].fill(couleur)
+    window.blit(globals()[nom], globals()[nom_xy])
+    elements[nom] = nom_xy
+"""
+A CORRIGER
+def creer_texte(nom, string, couleur, x, y, car_invisible=False):
+    nom_xy = nom+"_xy"
+    globals()[nom_xy] = xy_pourcent(x, y)
+    globals()[nom] = impact.render(string, car_invisible, couleur)
+    window.blit(globals()[nom], globals()[nom_xy])
+    elements[nom] = nom_xy
+# Creation des textes
+creer_texte("texte_score", "meilleur score = 0 mpm", white, 1, 1.4)
+creer_texte("texte_temps", "30sec", white, 83, 30)
+"""
 def charger_formes():
     # Afficher tout : label, texte...
     window.fill((0, 0, 0))
+    window.blit(ecriture, ecriture_xy)
     window.blit(score, score_xy)
     window.blit(temps, temps_xy)
     window.blit(mpm_actuel, mpm_actuel_xy)
     window.blit(exit, (exit_xy))
-    window.blit(texte_score, texte_score_xy)
+    #window.blit(texte_score, texte_score_xy)
+
+def xy_pourcent(X, Y): # x, y convertit selon taille de l'ecran en pourcent
+    return int(pygame.display.Info().current_w*X*0.01), int(pygame.display.Info().current_h *Y*0.01)
+
+def choisir_langue(langue): # L'utilisateur choisi sa langue = selection d'un fichier en consequence
+    global mots
+    fichiertxt = f"Langues/{langue}.txt"
+    with codecs.open(fichiertxt, encoding = 'utf-8') as fichier:
+        mots = [ligne[:-1] for ligne in fichier.readlines()]
     
-    
-# Création du label score
-score_xy, score_wh = xy_pourcent(0.5, 0.85), xy_pourcent(21.5, 8) # Position et taille
-score = pygame.Surface(score_wh) # Creer le label
-score.fill(green) # Remplir de couleur   
+# Creation des labels
+creer_label("ecriture",cyan ,15, 13, 65, 65)
+creer_label("score", green , 0.5, 0.85, 21.5, 8)
+creer_label("temps", blue , 83, 30, 15, 10)
+creer_label("mpm_actuel", white , 39.6, 87, 20, 10)
+creer_label("exit", red , 96, 0, 4, 6)
 
-# Création du label temps
-temps_xy, temps_wh = xy_pourcent(83, 30), xy_pourcent(15, 10)
-temps = pygame.Surface((temps_wh)) # Pareille que label score
-temps.fill(blue)
 
-# Création du label mpm_actuel
-mpm_actuel_xy, mpm_actuel_wh =  xy_pourcent(40, 87), xy_pourcent(20, 10)
-mpm_actuel = pygame.Surface(mpm_actuel_wh) # Pareille que label score
-mpm_actuel.fill(white)
 
-# Création du label exit
-exit_xy, exit_wh = xy_pourcent(96, 0), xy_pourcent(4, 6)
-exit = pygame.Surface((exit_wh)) # Pareille que label score
-exit.fill(red)
-
-# Polices d'ecriture
+# Creation des polices d'ecriture
 taille_police = 70
-comic_sans_ms = police('Comic Sans MS', taille_police)
-arial = police('Arial', taille_police)
-impact = police('impact', taille_police)
+comic_sans_ms = pygame.font.SysFont('Comic Sans MS', taille_police)
+arial = pygame.font.SysFont('Arial', taille_police)
+impact = pygame.font.SysFont('impact', taille_police)
 
-# Création d'un texte
-texte_score_xy = xy_pourcent(1, 1.4)
-texte_score = impact.render('Meilleur score', False, white)
 
-# loop back pour vérifier les évenements clavier
+
+
+
+# L'utilisateur choisi une langue
+langue_choisie = False
+while not langue_choisie:
+    langue_choisie = True
+
+
+# loop back pour verifier les evenements clavier
 continuer = True
 while continuer:
     for evenement in pygame.event.get():
@@ -78,16 +91,14 @@ while continuer:
                 print("bouton de temps clique ! ") # Mettre la fonction pour changer le temps de test
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
-        print("touche z cliqué")
+        print("touche z clique")
     if keys[pygame.K_s]:
-        print("touche s cliqué")
+        print("touche s clique")
         
 
     # Your game logic here
     
-    # Afficher tout : label, texte...
-    charger_formes()
-    pygame.display.flip()
+    # Probleme du alt+tab
+    charger_formes(), pygame.display.flip()
 
-pygame.quit()
-quit()
+pygame.quit(), quit()
